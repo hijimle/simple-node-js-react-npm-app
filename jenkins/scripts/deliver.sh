75f912a04e5b1e09f1ad2bff7933a974d3ab61b7
hijimle <hijimle@users.noreply.github.com>
2020-03-20 09:25:32 -0600

    InsertComment

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,7 +25,7 @@ npm start &
 sleep 1
 echo $! > .pidfile
 set +x
-
+curl http://localhost:3000
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
