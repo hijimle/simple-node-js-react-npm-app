31b23ea2b4afdae2ca6dae491a67706885f13922
hijimle <hijimle@users.noreply.github.com>
2020-03-22 12:46:47 -0600

    InsertComment

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -9,11 +9,11 @@ whoami;date
 set -x
 echo 'does not work - npm i @babel/core babel-loader @babel/preset-env @babel/preset-react @babel/plugin-proposal-numeric-separator --save-dev'
 echo 'replaced npm run build with yarn run build'
-cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/* /var/jenkins_home/workspace/jimle
+cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/durable\-*/*.txt /var/jenkins_home/workspace/jimle
 yarn run build
-cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/* /var/jenkins_home/workspace/jimle
+cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/durable\-*/*.txt /var/jenkins_home/workspace/jimle
 set +x
-
+cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/durable\-*/*.txt /var/jenkins_home/workspace/jimle
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
@@ -25,9 +25,9 @@ echo 'the file ".pidfile".'
 set -x
 echo 'replaced npm start & with yarn start &'
 yarn start &
-cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/* /var/jenkins_home/workspace/jimle
+cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/durable\-*/*.txt /var/jenkins_home/workspace/jimle
 sleep 1
-cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/* /var/jenkins_home/workspace/jimle
+cp -r /var/jenkins_home/workspace/simple-node-js-react-npm-app@tmp/durable\-*/*.txt /var/jenkins_home/workspace/jimle
 echo $! > .pidfile
 set +x
 curl http://localhost:5556
