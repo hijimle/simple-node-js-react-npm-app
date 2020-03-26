74624ed04fa02316e6057fef7bb39914546b497f
hijimle <hijimle@users.noreply.github.com>
2020-03-25 23:56:16 -0600

    InsertComment

--- src/App.js
+++ src/App.js
@@ -6,41 +6,41 @@ import './custom.scss';
 class App extends Component {
   render() {
     return (
-      <>
-        <Navbar bg="light">
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-        </Navbar>
-        <br />
-        <Navbar bg="light">
-          <Navbar.Brand>Brand text</Navbar.Brand>
-        </Navbar>
-        <br />
-        <Navbar bg="dark">
-          <Navbar.Brand href="#home">
-            <img
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        </Navbar>
-        <br />
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
-          </Navbar.Brand>
-        </Navbar>
-      </>
       <div className="App">
+        <>
+          <Navbar bg="light">
+            <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+          </Navbar>
+          <br />
+          <Navbar bg="light">
+            <Navbar.Brand>Brand text</Navbar.Brand>
+          </Navbar>
+          <br />
+          <Navbar bg="dark">
+            <Navbar.Brand href="#home">
+              <img
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+          </Navbar>
+          <br />
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{' '}
+              React Bootstrap
+            </Navbar.Brand>
+          </Navbar>
+        </>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
