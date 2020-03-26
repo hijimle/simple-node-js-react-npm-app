9165a6f1bb4519487a345bbbf1f14aa4d292fb99
hijimle <hijimle@users.noreply.github.com>
2020-03-26 01:28:02 -0600

    InsertComment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './App.scss';
-import DesignerNavbar from './DesignerNavbar/DesignerNavbar';
+import DesignerNavbar from './DesignerNavbar';
 
 class App extends Component {
   render() {
