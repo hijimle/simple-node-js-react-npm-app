65147b5d5261c00394693d1af8239d5f5bce3b30
hijimle <hijimle@users.noreply.github.com>
2020-03-26 01:07:43 -0600

    InsertComment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import 'react-bootstrap/dist/react-bootstrap.min.css';
+import './react-bootstrap/dist/react-bootstrap.min.css';
 import './App.css';
 import './App.scss';
 
