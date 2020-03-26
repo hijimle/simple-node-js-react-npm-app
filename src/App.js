3dd1aeaf22631577780b5c1b8609396b72f956b2
hijimle <hijimle@users.noreply.github.com>
2020-03-26 00:09:05 -0600

    InsertComment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
